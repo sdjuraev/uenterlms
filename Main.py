8e8fb557b13dd04a037fd776ad93eea68ca60de6
sdjuraev <s.djuraev@newuu.uz>
2025-06-30 17:09:58 +0500

    Com

--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
 from Book import Book
 from Loan import Loan
-from User import User
-from  MainMenu import  Menu
 from  LibrarySystem import LibrarySystem
 from Librarian import Librarian
 from datetime import date
@@ -9,6 +7,7 @@ from datetime import date
 if __name__ == '__main__':
     librarySystem = LibrarySystem()
     book = Book(1222, "Utgan kunlar", "Abdulla Qodiriy",1212,"Drama",True,'','')
+    book2 = Book(111,"Something", "Avtor",True)
     librarian = Librarian('jessy',"Jessy", 'pass','email',librarySystem)
     librarySystem.addBook(book)
     librarian.showBooks()
